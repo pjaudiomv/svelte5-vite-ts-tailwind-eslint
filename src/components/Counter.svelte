02c777ded5025cafed7feefb9615c9ae570ab6f6
Patrick Joyce <pjaudiomv@gmail.com>
2025-06-03 15:46:26 -0400

    add dropdown

--- src/components/Counter.svelte
+++ src/components/Counter.svelte
@@ -1,4 +1,13 @@
 <script lang="ts">
+  import { Select, Label } from 'flowbite-svelte';
+
+  let selectedCharacter = $state('');
+  let characters = [
+    { value: 'mcfly', name: 'Marty McFly' },
+    { value: 'doc', name: 'Emmett "Doc" Brown' },
+    { value: 'biff', name: 'Biff Tannen' }
+  ];
+
   let count: number = $state(0);
   let isTimeTraveling = $state(false);
   let showLightning = $state(false);
@@ -48,6 +57,10 @@
     <span class="mr-2 mb-2 inline-block rounded-full bg-pink-100 px-3 py-1 text-sm font-semibold text-pink-800 transition-colors duration-200 hover:bg-pink-200">#delorean</span>
     <span class="mr-2 mb-2 inline-block rounded-full bg-blue-100 px-3 py-1 text-sm font-semibold text-blue-800 transition-colors duration-200 hover:bg-blue-200">#88</span>
   </div>
+  <Label>
+    Select Character
+    <Select class="mt-2" placeholder="Choose character ..." items={characters} bind:value={selectedCharacter} />
+  </Label>
 </div>
 
 <style>
--- src/tests/App.svelte.test.ts
+++ src/tests/App.svelte.test.ts
@@ -1,6 +1,7 @@
 import { describe, test, expect } from 'vitest';
 import '@testing-library/jest-dom/vitest';
 import { render, screen } from '@testing-library/svelte';
+import userEvent from '@testing-library/user-event';
 
 import App from '../App.svelte';
 
@@ -8,5 +9,12 @@ describe('Counter', () => {
   test('initial screen', async () => {
     render(App);
     expect(screen.getByText('Great Scott!')).toBeInTheDocument();
+  });
+
+  test('select dropdown', async () => {
+    render(App);
+    const countryBox = screen.getByRole('combobox', { name: 'Select Character' }) as HTMLSelectElement;
+    expect(countryBox.item(0)?.label).toBe('Choose character ...');
+    await userEvent.selectOptions(countryBox, ['Marty McFly']);
   });
 });
